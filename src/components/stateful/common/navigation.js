3052bf2846869652f850a1070df41e7f0264d0cc
Mentor Palokaj <mentor@palokaj.co>
2020-03-27 13:20:04 +0100

    Sidebar slidein with animation works

--- src/components/stateful/common/navigation.js
+++ src/components/stateful/common/navigation.js
@@ -1,28 +1,74 @@
 import React from 'react'
+import { Animated } from 'react-native'
 import { Component } from '../../stateless/common/generic'
 import { Header, Menu } from '../../stateless/common/navigation'
 import { connect } from 'react-redux'
-import { capitalize } from '../../../modules/helpers'
+import { capitalize, log } from '../../../modules/helpers'
 
 class Navigation extends Component {
 
-	constructor( props ) {
-		super( props )
-		this.state = {
-			drawer: false
-		}
-
+	state = {
+		drawer: false,
+		drawerWidth: 500,
+		drawerSpeed: 500
 	}
 
-	toggleDrawer = f => this.updateState( { drawer: !this.state.drawer } )
+	// Drawer toggler
+	toggleDrawer = async config => {
+
+		const { drawer, drawerWidth, drawerSpeed } = this.state
+		const force = config == 'force'
+
+		// If the drawer is open, move it to closed position
+		if( drawer && !force ) Animated.timing( this.pan, { toValue: { x: drawerWidth, y: 0 }, duration: drawerSpeed } ).start( f => this.updateState( { drawer: !drawer } ) )
+		
+		// If drawer is closed, dirst mount, then animate open
+		else if( !drawer || force ) {
+			if( !force ) await this.updateState( { drawer: !drawer } )
+			Animated.timing( this.pan, { toValue: { x: 0, y: 0 }, duration: drawerSpeed } ).start()
+		}
+	}
+
+	// The animated .event function returns a function
+	pan = new Animated.ValueXY( { x: this.state.drawerWidth, y: 0 } )
+	handleDrag = Animated.event(
+		[ { translationX: this.pan.x } ],
+		{ useNativeDriver: true }
+	)
+
+	panDrawer = ( { nativeEvent } ) => {
+
+		const { translationX, velocityX, state } = nativeEvent
+		const { drawerWidth } = this.state
+
+		// If drag is not on x axis or is to the left of base position ignore
+		if( !translationX || translationX < 0 ) return
+
+		// Animate if state is pulling
+		if( state == 4 ) return this.handleDrag( nativeEvent )
+
+		// If we let go, either reset or hide
+		if( state == 5 && ( translationX > drawerWidth / 5 || velocityX > 2 ) ) return this.toggleDrawer()
+		else return this.toggleDrawer( 'force' )
+		
+		
+
+	}
 
 	render( ) {
 
 		const { title } = this.props
-		const { drawer } = this.state
-		console.log( 'Drawer ', drawer )
+		const { drawer, drawerWidth, drawerOffset } = this.state
 
-		return <Header toggle={ this.toggleDrawer } title={ capitalize( title ) } drawer={ drawer } />
+		return <Header
+			drawerTranslate={ { transform: this.pan.getTranslateTransform() } }
+			drawerOffset={ drawerOffset }
+			drawerWidth={ drawerWidth }
+			pan={ this.panDrawer }
+			toggle={ this.toggleDrawer } 
+			title={ capitalize( title ) }
+			drawer={ drawer }
+		/>
 	}
 
 }
--- src/components/stateless/common/navigation.js
+++ src/components/stateless/common/navigation.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { TouchableOpacity, View, Text } from 'react-native'
+
+// Visual
+import { TouchableOpacity, View, Text, Animated } from 'react-native'
 import { Drawer, Portal, Appbar, withTheme, Surface } from 'react-native-paper'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+
 
 // Expo dependencies
 import Constants from 'expo-constants'
@@ -8,38 +12,73 @@ import Constants from 'expo-constants'
 // ///////////////////////////////
 // Navigation
 // ///////////////////////////////
-export const Header = ( { style, back, title, subtitle, toggle, drawer, children } ) => <View style={ { width: '100%' } }>
+export const Header = ( { style, back, title, subtitle, toggle, pan, drawer, drawerWidth, drawerTranslate, children } ) => <View style={ { width: '100%' } }>
 	<Appbar.Header style={ { width: '100%', paddingVertical: 30, ...( !back && { paddingLeft: 20 } ), ...style }} statusBarHeight={ Constants.statusBarHeight }>
 		{ back && <Appbar.BackAction onPress={ back } /> }
 		<Appbar.Content title={ title } subtitle={ subtitle }/>
 		{ children }
 		<Appbar.Action icon="menu" onPress={ toggle } />
 	</Appbar.Header>
-	{ drawer && <Menu toggle={ toggle } /> }
+	{ drawer && <Menu translate={ drawerTranslate } width={ drawerWidth } pan={ pan } toggle={ toggle } /> }
 </View>
 
 // Sidebar
-export const Menu = withTheme( ( { children, theme, toggle, ...props } ) => <Portal style={ { alignItems: 'center', justifyContent: 'center' } }>
+export const Menu = withTheme( ( { width, children, theme, toggle, pan, translate, ...props } ) => <Portal style={ { alignItems: 'center', justifyContent: 'center' } }>
 
-	<TouchableOpacity onPress={ toggle } style={ { flex: 1 } }>
+	<TouchableOpacity activeOpacity={ 1 } onPress={ toggle } style={ { flex: 1 } }>
 
-		<TouchableOpacity style={ { height: '100%', width: 500, maxWidth: '100%', alignSelf: 'flex-end' } } onPress={ e => e }>
-			<Surface style={ { flex: 1 } }>
-				<Drawer.Section title='Menu' style={ { height: '100%' } }>
-				<Drawer.Item
-			      label="First Item"
-			      active={ true }
-			      onPress={ f => f }
-			    />
-			    <Drawer.Item
-			      label="Second item"
-			      active={ false }
-			      onPress={ f => f }
-			    />
-			    </Drawer.Section>
-			</Surface>
+		<TouchableOpacity activeOpacity={ 1 } style={ { height: '100%', width: width, maxWidth: '100%', alignSelf: 'flex-end' } } onPress={ e => e.preventDefault() }>
+
+			<PanGestureHandler onGestureEvent={ pan }>
+
+				<Animated.View style={ [ translate, { flex: 1 } ] }>
+
+					<Surface style={ { flex: 1 } }>
+						<Drawer.Section title='Menu' style={ { height: '100%', marginBottom: 0 } }>
+						<Drawer.Item
+					      label="First Item"
+					      active={ true }
+					      onPress={ f => f }
+					    />
+					    <Drawer.Item
+					      label="Second item"
+					      active={ false }
+					      onPress={ f => f }
+					    />
+					    </Drawer.Section>
+					</Surface>
+
+				</Animated.View>
+				
+			</PanGestureHandler>
+
 		</TouchableOpacity>
 		
 	</TouchableOpacity>	
 	
 </Portal> )
+
+// export const Menu = withTheme( ( { children, theme, toggle, ...props } ) => <Portal style={ { alignItems: 'center', justifyContent: 'center' } }>
+
+// 	<PanGestureHandler onGestureEvent={ e => console.log( e ) } style={ { flex: 1, backgroundColor: 'red' } }>
+
+// 		<View style={ { height: '100%', width: 500, maxWidth: '100%', alignSelf: 'flex-end' } }>
+// 			<Surface style={ { flex: 1 } }>
+// 				<Drawer.Section title='Menu' style={ { height: '100%' } }>
+// 				<Drawer.Item
+// 			      label="First Item"
+// 			      active={ true }
+// 			      onPress={ f => f }
+// 			    />
+// 			    <Drawer.Item
+// 			      label="Second item"
+// 			      active={ false }
+// 			      onPress={ f => f }
+// 			    />
+// 			    </Drawer.Section>
+// 			</Surface>
+// 		</View>
+		
+// 	</PanGestureHandler>	
+	
+// </Portal> )
