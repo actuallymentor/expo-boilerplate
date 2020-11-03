023464694aabe84a43bd1fa7b84d467d5fefdb44
Mentor Palokaj <mentor@palokaj.co>
2020-11-03 11:13:16 +0100

    Big in snap to docs

--- src/modules/firebase/helpers.js
+++ src/modules/firebase/helpers.js
@@ -3,7 +3,7 @@ import { catcher, error } from '../helpers'
 export const dataFromSnap = snapOfDocOrDocs => {
 
 	// If these are multiple docs
-	if( snapOfDocOrDocs.docs ) return snapOfDocOrDocs.docs( doc => ( { id: doc.id, ...doc.data( ) } ) )
+	if( snapOfDocOrDocs.docs ) return snapOfDocOrDocs.docs.map( doc => ( { id: doc.id, ...doc.data( ) } ) )
 
 	// If this is a single document
 	return { ...snapOfDocOrDocs.data(), id: snapOfDocOrDocs.id }
