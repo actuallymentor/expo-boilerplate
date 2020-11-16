7751062a417e558f3f3756946d401336f13f4a62
Mentor Palokaj <mentor@palokaj.co>
2020-11-16 15:29:09 +0100

    Default deps

--- .ncurc.js
+++ .ncurc.js
@@ -5,6 +5,7 @@ module.exports = {
     "react-native-dotenv", // Version 0.2.0 is fine, anything above that changes structure and does not work with expo web
     "history", // History of router v5 is nto yet compatible with history v5
     "react", // Managed by expo
+    "react-dom", // Managed by expo
     "react-native", // Managed by expo
     "react-native-web", // managed by expo
   ]
