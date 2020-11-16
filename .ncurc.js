bb8bd1aea75ecbba532ffd73b68859bd7be3e5ee
Mentor Palokaj <mentor@palokaj.co>
2020-11-16 15:23:31 +0100

    react not as ncurc

--- .ncurc.js
+++ .ncurc.js
@@ -4,7 +4,7 @@ module.exports = {
   "reject": [
     "react-native-dotenv", // Version 0.2.0 is fine, anything above that changes structure and does not work with expo web
     "history", // History of router v5 is nto yet compatible with history v5
-    "react",
+    "react", // Managed by expo
     "react-native", // Managed by expo
     "react-native-web", // managed by expo
   ]
