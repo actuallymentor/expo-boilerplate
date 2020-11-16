394c0919ff3a917ad7b6240615738abde76b60cd
Mentor Palokaj <mentor@palokaj.co>
2020-11-16 15:20:55 +0100

    Update ncurc

--- .ncurc.js
+++ .ncurc.js
@@ -1,7 +1,11 @@
 // This file governs the ncu utility that does auto upgrades
 module.exports = {
-  "upgrade": true,
+  // "upgrade": false,
   "reject": [
-    "react-native-dotenv" // Version 0.2.0 is fine, anything above that changes structure and does not work with expo web
+    "react-native-dotenv", // Version 0.2.0 is fine, anything above that changes structure and does not work with expo web
+    "history", // History of router v5 is nto yet compatible with history v5
+    "react",
+    "react-native", // Managed by expo
+    "react-native-web", // managed by expo
   ]
 }
