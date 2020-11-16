99e847ee353b725b6dd31d485ace0a920649958a
Mentor Palokaj <mentor@palokaj.co>
2020-11-16 15:06:09 +0100

    Editing sentry formatting

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,5 @@
 // This file governs the ncu utility that does auto upgrades
-{
+module.exports = {
   "upgrade": true,
   "reject": [
     "react-native-dotenv" // Version 0.2.0 is fine, anything above that changes structure and does not work with expo web
---
+++ src/modules/apis/sentry.js
@@ -0,0 +1,12 @@
+import * as ExpoSentry from 'sentry-expo'
+import { isWeb } from './platform'
+import { SENTRY_DSN } from 'react-native-dotenv'
+
+export const SentryInit = f => {
+	if( SENTRY_DSN ) ExpoSentry.init( {
+	  dsn: SENTRY_DSN
+	  // enableInExpoDevelopment: true
+	} )
+}
+
+export const Sentry = isWeb ? ExpoSentry.Browser : ExpoSentry.Native
